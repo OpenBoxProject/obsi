11e8273c84c67532550b3e9eafead9c5693d08e6
Pavel Lazar <pavel.lazar@gmail.com>
2015-12-21 19:35:17 +0200

    Install Script: Add sudo for install

--- install.sh
+++ install.sh
@@ -4,17 +4,22 @@ BUILD_DIR="/tmp/build"
 RE2_URL="https://github.com/google/re2.git"
 RE2_TAG="2015-11-01"
 
+function install_build_utils {
+	sudo apt-get install g++
+}
 function install_re2 {
 	echo "[+] Clonning RE2"
 	cd $BUILD_DIR
 	git clone $RE2_URL
 	cd re2
 	git checkout $RE2_TAG
-	echo "[+] Installing RE2"
+	echo "[+] Compiling RE2"
 	make 
+	echo "[+] Testing RE2"
 	make test
-	make install 
-	make testinstall
+	echo "[+] Installing RE2"
+	sudo make install 
+	sudo make testinstall
 }
 
 rm -rf $BUILD_DIR
